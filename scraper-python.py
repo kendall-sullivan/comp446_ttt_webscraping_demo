e07a3263539c7ad64f6cb2f42fe92b0cdca5914b
kendall-sullivan <123098060+kendall-sullivan@users.noreply.github.com>
2025-03-12 20:05:18 +0100

    Create scraper-python.py

---
+++ scraper-python.py
@@ -0,0 +1,25 @@
+# Code Author: Joe Osborne (https://github.com/thejoeosborne/simple-scraper/blob/main/scraper-python.py)
+# scraper-python.py
+# To run this script, paste `python scraper-python.py` in the terminal
+
+import requests
+from bs4 import BeautifulSoup
+
+def scrape():
+
+    url = 'https://www.example.com'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    print(soup)
+
+    title = soup.select_one('h1').text
+    text = soup.select_one('p').text
+    link = soup.select_one('a').get('href')
+
+    print(title)
+    print(text)
+    print(link)
+
+
+if __name__ == '__main__':
+    scrape()
